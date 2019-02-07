d038836a57c5701c35fca4f7b2cec86889eb4569
Les Zychowski <les.zychowski@auth0.com>
2019-02-07 14:38:18 -0600

    added redirects and cognito link

--- src/App.js
+++ src/App.js
@@ -3,20 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 import Input from './components/input';
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <Router>
+        <div className="App">
+          <header className="App-header">
 
-          <Input name="email" label="Email" type="text"></Input>
-          <Input name="password" label="Password" type="password"></Input>
-          <input type="submit"></input>
-          
-        </header>
-      </div>
+            {/* <Input name="email" label="Email" type="text"></Input>
+            <Input name="password" label="Password" type="password"></Input>
+            <input type="submit"></input> */}
+
+            <Route exact path="/" component={Home} />
+            <Route path="/callback" component={Callback} />
+            
+          </header>
+        </div>
+      </Router>
+      
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <a href="https://les-hackday.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=2cqihi71uocc67ijkekos2t9jm&redirect_uri=https://zen-jepsen-8d8056.netlify.com/callback" style={{color: "white"}}>Login</a>
+  </div>
+)
+
+const Callback = () => (
+  <div>
+    Welcome
+  </div>
+)
 
 export default App;
